ae10c02452a4dfbe0219ff3412294c54102d34ac
Craig Olson <craig.olson@fooda.com>
2019-06-03 14:31:04 -0500

    Initial Commit

---
+++ graphql-ios/graphql_ios.h
@@ -0,0 +1,19 @@
+//
+//  graphql_ios.h
+//  graphql-ios
+//
+//  Created by Craig Olson on 6/3/19.
+//  Copyright © 2019 Fooda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for graphql_ios.
+FOUNDATION_EXPORT double graphql_iosVersionNumber;
+
+//! Project version string for graphql_ios.
+FOUNDATION_EXPORT const unsigned char graphql_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <graphql_ios/PublicHeader.h>
+
+
